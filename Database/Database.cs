4393db86e6201e1656833884723e258982f11e79
Milan Jeremic <milanjeremic@LIVE.COM>
2021-05-10 15:13:00 +0200

    Implementing CREATE TABLE and INSERT INTO statement.

--- Database/Database.cs
+++ Database/Database.cs
@@ -67,6 +67,52 @@ namespace Database
         public void ProcessQuery(string query)
         {
             Console.WriteLine("Received query: " + query);
+
+            const string CreateTableStatement = "CREATE TABLE ";
+            const string InsertIntoTableStatementStart = "INSERT INTO TABLE ";
+            const string InsertIntoTableStatementValues = "VALUES";
+
+            string tableName = null;
+
+            switch (query)
+            {
+                case string s when s.StartsWith(CreateTableStatement):
+                    tableName = query.Substring(CreateTableStatement.Length).Trim();
+                    if (!tableName.All(char.IsLower))
+                    {
+                        throw new Exception("Invalid table name.");
+                    }
+
+                    CreateTable(tableName);
+                    break;
+
+                case string s when s.StartsWith(InsertIntoTableStatementStart):
+                    string insertIntoStatementPart = query.Substring(InsertIntoTableStatementStart.Length).Trim();
+
+                    tableName = insertIntoStatementPart.Substring(0, insertIntoStatementPart.IndexOf(" "));
+                    Table table = GetTable(tableName);
+                    if (table == null)
+                    {
+                        throw new Exception("Table not found for insert statement.");
+                    }
+
+                    insertIntoStatementPart = insertIntoStatementPart.Substring(tableName.Length)
+                        .SkipWhile(char.IsWhiteSpace).ToString();
+                    if (!insertIntoStatementPart.StartsWith(InsertIntoTableStatementValues))
+                    {
+                        throw new Exception("Syntax error. VALUES expected.");
+                    }
+
+                    string valuesPart = insertIntoStatementPart.Substring(InsertIntoTableStatementValues.Length);
+
+                    string[] values = valuesPart.Split(',');
+                    foreach (string value in values)
+                    {
+                        table.Insert(int.Parse(value.Trim()));
+                    }
+
+                    break;
+            }
         }
     }
 }
