9ab2c155c161240e0821e5b642e83324107fbbc4
Milan Jeremic <milanjeremic@LIVE.COM>
2021-05-23 14:03:22 +0200

    Added CHECK table syntax for testing

--- Database/Database.cs
+++ Database/Database.cs
@@ -84,15 +84,19 @@ namespace Database
             return table;
         }
 
+        private const string CreateTableStatement = "CREATE TABLE ";
+        private const string SelectFromTableStatement = "SELECT FROM ";
+
+        private const string InsertIntoTableStatementStart = "INSERT INTO ";
+        private const string CheckTableStatement = "CHECK ";
+        private const string ValuesStatementPart = "VALUES";
+
         public string ProcessQuery(string query)
         {
             // Console.WriteLine("Received query: " + query);
 
-            const string CreateTableStatement = "CREATE TABLE ";
-            const string InsertIntoTableStatementStart = "INSERT INTO ";
-            const string InsertIntoTableStatementValues = "VALUES";
-            const string SelectFromTableStatement = "SELECT FROM ";
-
+            Table table = null;
+            List<int> values = null;
             string tableName = null;
             string result = null;
 
@@ -110,26 +114,42 @@ namespace Database
                     break;
 
                 case string s when s.StartsWith(InsertIntoTableStatementStart):
-                    string insertIntoStatementPart = query.Substring(InsertIntoTableStatementStart.Length).Trim();
+                    (table, values) = ParseInsertOrCheckTableStatement(query, InsertIntoTableStatementStart);
 
-                    tableName = insertIntoStatementPart.Substring(0, insertIntoStatementPart.IndexOf(" "));
-                    Table table = GetExistingTable(tableName);
-
-                    insertIntoStatementPart = insertIntoStatementPart.Substring(tableName.Length).Trim();
-                    if (!insertIntoStatementPart.StartsWith(InsertIntoTableStatementValues))
-                    {
-                        throw new Exception("Syntax error. VALUES expected.");
-                    }
-
-                    string valuesPart = insertIntoStatementPart.Substring(InsertIntoTableStatementValues.Length);
-
-                    int[] values = valuesPart.Split(',').Select(value => int.Parse(value.Trim())).ToArray();
                     foreach (int value in values)
                     {
                         table.Insert(value);
                     }
 
                     Console.WriteLine(string.Format("Added [{0}] to table {1}", string.Join(", ", values), tableName));
+                    break;
+
+                case string s when s.StartsWith(CheckTableStatement):
+                    (table, values) = ParseInsertOrCheckTableStatement(query, CheckTableStatement);
+
+                    List<int> missingValues = new List<int>();
+                    foreach (int tableValue in table.Values)
+                    {
+                        if (values.Contains(tableValue))
+                        {
+                            values.Remove(tableValue);
+                        }
+                        else
+                        {
+                            missingValues.Add(tableValue);
+                        }
+                    }
+
+                    if (values.Any())
+                    {
+                        throw new Exception("Table missing elements: " + string.Join(", ", values));
+                    }
+
+                    if (missingValues.Any())
+                    {
+                        throw new Exception("Table contains extra elements: " + string.Join(", ", missingValues));
+                    }
+
                     break;
 
                 case string s when s.StartsWith(SelectFromTableStatement):
@@ -147,6 +167,31 @@ namespace Database
             }
 
             return result;
+        }
+
+        private (Table, List<int>) ParseInsertOrCheckTableStatement(string query, string prefix)
+        {
+            string statementPart = query.Substring(prefix.Length).Trim();
+
+            string tableName = statementPart.Substring(0, statementPart.IndexOf(" "));
+            Table table = GetExistingTable(tableName);
+
+            statementPart = statementPart.Substring(tableName.Length).Trim();
+            string valuesPart = ParseOutValues(statementPart);
+
+            List<int> values = valuesPart.Split(',').Select(value => int.Parse(value.Trim())).ToList();
+
+            return (table, values);
+        }
+
+        private string ParseOutValues(string partWithValues)
+        {
+            if (!partWithValues.StartsWith(ValuesStatementPart))
+            {
+                throw new Exception("Syntax error. VALUES expected.");
+            }
+
+            return partWithValues.Substring(ValuesStatementPart.Length).Trim();
         }
     }
 }
