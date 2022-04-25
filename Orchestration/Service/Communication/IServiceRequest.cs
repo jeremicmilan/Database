8cc2a675d3a5d26cbba1a46ba2db1adaadcbf5b7
Milan Jeremic <milanjeremic@LIVE.COM>
2022-04-25 07:46:42 +0200

    IServiceRequest -> public

--- Orchestration/Service/Communication/IServiceRequest.cs
+++ Orchestration/Service/Communication/IServiceRequest.cs
@@ -1,6 +1,8 @@
 ﻿namespace Database
 {
-    internal interface IServiceRequest
+    // Unfortunately we need the Interface, as we cannot have a reference to a generic base class (of actual non-generic derived class)
+    //
+    public interface IServiceRequest
     {
         public abstract ServiceResponseResult Process();
     }
