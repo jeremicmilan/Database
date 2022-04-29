24e86d74a2580f57b3faeee2316a9fa9b6c4fb01
Milan Jeremic <milanjeremic@LIVE.COM>
2022-04-29 12:32:02 +0200

    Log manager should read log from disk on stratup in case it gets restarted.

--- Orchestration/Service/LogService/LogService.cs
+++ Orchestration/Service/LogService/LogService.cs
@@ -23,7 +23,9 @@ namespace Database
             // Log service only processes the requests from Database Service and Storage Service
             // in the current implementation. In the real world, log service would do much more.
             // One example is log destage to cheaper storage for point in time restore.
+            // For now just read what you found on disk in case of log manager restart.
             //
+            LogManager.ReadEntireLog();
         }
 
         public override void SnapWindow()
