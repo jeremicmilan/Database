7415428de3513f83265f798805628354debcf58a
Milan Jeremic <milanjeremic@LIVE.COM>
2022-07-21 21:13:32 +0200

    DataFilePath not used in DatabaseTraditional

--- Database/DatabaseTraditional.cs
+++ Database/DatabaseTraditional.cs
@@ -2,16 +2,12 @@
 {
     public class DatabaseTraditional : Database
     {
-        public string DataFilePath { get; private set; }
-
         private DatabaseTraditional(DatabaseService databaseService, string logPath, string dataPath)
             : base(
                   databaseService,
                   new LogManagerTraditional(logPath),
                   new StorageManagerTraditional(dataPath))
-        {
-            DataFilePath = dataPath ?? Utility.DefaultDataFilePath;
-        }
+        { }
 
         public static DatabaseTraditional Create(
             DatabaseService databaseService,
