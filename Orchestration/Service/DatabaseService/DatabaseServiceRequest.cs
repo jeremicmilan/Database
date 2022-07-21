b65710ad76da64c871d26d669a41001eccd7641f
Milan Jeremic <milanjeremic@LIVE.COM>
2022-07-21 21:14:38 +0200

    DatabaseServiceRequest should enforse the generic DatabaseServiceResponseResult

--- Orchestration/Service/DatabaseService/DatabaseServiceRequest.cs
+++ Orchestration/Service/DatabaseService/DatabaseServiceRequest.cs
@@ -2,8 +2,8 @@
 
 namespace Database
 {
-    public abstract class DatabaseServiceRequest<TServiceResponseResult> : ServiceRequest<TServiceResponseResult>
-        where TServiceResponseResult : ServiceResponseResult
+    public abstract class DatabaseServiceRequest<TDatabaseServiceResponseResult> : ServiceRequest<TDatabaseServiceResponseResult>
+        where TDatabaseServiceResponseResult : DatabaseServiceResponseResult
     {
         protected override Type GetServiceType() => typeof(DatabaseService);
     }
