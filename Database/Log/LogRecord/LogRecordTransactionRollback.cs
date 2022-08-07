1d135a4632467e967fa205ce3ccd451e1fdc7566
Milan Jeremic <milanjeremic@LIVE.COM>
2022-08-07 22:52:48 +0200

    LogRecordTransactionRollback was missed by git, so adding it now.

---
+++ Database/Log/LogRecord/LogRecordTransactionRollback.cs
@@ -0,0 +1,17 @@
+﻿namespace Database
+{
+    public class LogRecordTransactionRollback : LogRecordTransaction
+    {
+        public LogRecordTransactionRollback()
+        { }
+
+        public LogRecordTransactionRollback(int logSequenceNumber, string[] parameters)
+            : base(logSequenceNumber)
+        { }
+
+        protected override void RedoInternal()
+        {
+            TransactionManager.Get().RollbackTransaction(redo: true);
+        }
+    }
+}
